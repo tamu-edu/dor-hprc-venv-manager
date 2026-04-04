c0e471319f6cda332f417892dd96f2c88244799a
Thang Ha <thanghn90@gmail.com>
2026-04-04 16:07:15 -0500

    ModuLair lua file
    Put this lua file within lmod's MODULEPATH (e.g. /sw/hprc/mods/all/Core/ModuLair). This module will create a wrapper function modulair() which automatically add "source" to "activate" and "deactivate" command. Currently supporting BASH shell only, no C Shell yet.

---
+++ lmod/ModuLair/1.0.lua
@@ -0,0 +1,38 @@
+help([==[
+
+Description
+===========
+ModuLair is a Python Virtual Environment management tool written by TAMU HPRC.
+
+More information
+================
+ - Homepage: https://hprc.tamu.edu/kb/Software/ModuLair/
+]==])
+
+conflict("ModuLair")
+add_property("lmod","sticky")
+
+whatis([==[Description: ModuLair is a Python Virtual Environment management tool written by TAMU HPRC.]==])
+whatis([==[Homepage: https://hprc.tamu.edu/kb/Software/ModuLair/]==])
+help([[ModuLair is a Python Virtual Environment management tool written by TAMU HPRC. Homepage: https://hprc.tamu.edu/kb/Software/ModuLair/]])
+
+local root = "/sw/hprc/sw/dor-hprc-venv-manager/"
+
+prepend_path("PATH", pathJoin(root, "bin"))
+
+local bashStr = [[
+\local cmd="${1-__missing__}"
+case "$cmd" in
+    activate|deactivate)
+        source modulair-cli "$@"
+        ;;
+    *)
+        modulair-cli "$@"
+        ;;
+esac
+]]
+
+local cshStr = 'echo "ModuLair does not support bash/tcsh at the moment. Please use bash."'
+
+set_shell_function("modulair",bashStr,cshStr)
+
